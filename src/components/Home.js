7616d14808e7463ea33ffe2f3ad69824911c6a65
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-09 11:59:48 +0200

    imported uuid

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import db from '../firebase'
 import { Link } from 'react-router-dom'
+import uuid from 'uuid/v4'
 
 class Home extends Component {
   render() {
