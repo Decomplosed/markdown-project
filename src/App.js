d9b1f7d5c56eb6a8f23795b667d0c96a204c1ef5
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-09 11:40:57 +0200

    imported Home and Post components to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import Home from './components/Home'
+import Post from './components/Post'
+
 function App() {
   return (
     <div className='container'>
