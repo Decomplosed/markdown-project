3c4a87e36bef98171c96e5024c3489da3622ac5f
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-09 13:06:03 +0200

    defined contructor

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,6 +4,10 @@ import db from '../firebase'
 import ReactMarkdown from 'react-markdown'
 
 class Post extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <div>
