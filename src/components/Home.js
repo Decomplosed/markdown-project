81939c48e4aa63a1459ce178f287722f6d028bad
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-09 12:12:31 +0200

    check if the db is connected properly

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,21 @@ class Home extends Component {
     title: '',
   }
 
+  componentDidMount() {
+    db.ref('/posts').on('value', (snapshot) => {
+      let posts = []
+      snapshot.forEach((childSnapshot) => {
+        posts.push({
+          ...childSnapshot.val(),
+          key: childSnapshot.key,
+        })
+      })
+      this.setState({ posts })
+    })
+  }
+
   render() {
+    console.log(this.state.posts)
     return (
       <div>
         <h1>Home</h1>
