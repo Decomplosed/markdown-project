bfb2f3f3db0d25836bdb2d79ae2fd1b0cf3dc76f
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-10 21:34:43 +0200

    imported stylesheet

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Post from './components/Post'
 
+import './App.css'
+
 function App() {
   return (
     <div className='container'>
